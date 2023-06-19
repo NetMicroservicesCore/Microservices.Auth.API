da665975f6b8766b7c1682a57b6b1f399c5b135e
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-06-19 15:00:54 -0600

    se agregan metodos de regsitro y login

---
+++ Controllers/AuthAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuPlaza.Compras.Pedidos.AuthAPI.Controllers
+{
+    [Route("api/[auth]")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+
+
+        [HttpPost("registro")]
+        public async Task<IActionResult> Registro()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+
+    }
+}
