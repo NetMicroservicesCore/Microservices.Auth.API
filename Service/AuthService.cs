95d12706da15eb57a46cf31f64c3b1eef96e967d
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-06-19 15:45:05 -0600

    se agrega  la clase AuthService implementando su interfaz con los emtodos adecuados para el login y registro

---
+++ Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SuPlaza.Compras.Pedidos.AuthAPI.Data;
+using SuPlaza.Compras.Pedidos.AuthAPI.Models;
+using SuPlaza.Compras.Pedidos.AuthAPI.Models.Dto;
+using SuPlaza.Compras.Pedidos.AuthAPI.Service.IService;
+
+namespace SuPlaza.Compras.Pedidos.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManaManager;
+
+
+        public AuthService(AppDbContext dbContext, UserManager<ApplicationUser> userManaManager)
+        {
+            _dbContext = dbContext;
+            _userManaManager = userManaManager;
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
