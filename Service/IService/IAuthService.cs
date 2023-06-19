33f874ee8c83c6dccd9ece372240e61623b87323
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-06-19 15:37:09 -0600

    se agrega intefaz de registro y logeo  para el  acceso a la api.

---
+++ Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using SuPlaza.Compras.Pedidos.AuthAPI.Models.Dto;
+
+namespace SuPlaza.Compras.Pedidos.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
