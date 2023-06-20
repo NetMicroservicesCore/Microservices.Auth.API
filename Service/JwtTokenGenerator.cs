7cb2a56a8874cb6f691393d7887abd34b7959d84
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-06-20 15:44:01 -0600

    se construye el metodo que se encarga de generar el token de seguridad.

---
+++ Service/JwtTokenGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SuPlaza.Compras.Pedidos.AuthAPI.Models;
+using SuPlaza.Compras.Pedidos.AuthAPI.Service.IService;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SuPlaza.Compras.Pedidos.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+
+            var tokenHandler= new JwtSecurityTokenHandler();
+            //obtenemos el secreto del token
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+            var claimsList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub,applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name,applicationUser.Name.ToString()),
+
+            };
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimsList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
