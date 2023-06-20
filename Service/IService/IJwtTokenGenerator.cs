ae757c401eb0abf4267ad5d910b232e554f7ddbd
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-06-20 15:23:37 -0600

    se genera interface para la administracion de los token de seguridad

---
+++ Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using SuPlaza.Compras.Pedidos.AuthAPI.Models;
+
+namespace SuPlaza.Compras.Pedidos.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
